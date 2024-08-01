90aa70cb21387fbc12dd17e88b2446b8722bd0a1
Ghanshyam Agrawal <ghanshyamagrawal456@gmail.com>
2024-08-01 11:43:35 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import { Route, Routes} from "react-router-dom";
-import {DataProvider} from "./Context/DataContext";
-import Home from "./Home";
-import Navbar from "./Navbar";
+import React, { useEffect } from 'react';
+import { Route, Routes, HashRouter } from 'react-router-dom';
+import { DataProvider } from './Context/DataContext';
+import Home from './Home';
+import Navbar from './Navbar';
 import OurInfo from './OurInfo';
-import Footer from "./Footer";
-import LastFooter from "./LastFooter";
-import Apply from "./Apply";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { BrowserRouter as Router } from 'react-router-dom';
- 
+import Footer from './Footer';
+import LastFooter from './LastFooter';
+import Apply from './Apply';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 function App() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  useEffect(()=>{
-    navigate('/');
-  },[navigate])
+
   useEffect(() => {
-      window.scrollTo(0, 0);
+    navigate('/');
+  }, [navigate]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
   }, [pathname]);
+
   return (
-    <div className="App">
+    <HashRouter>
       <DataProvider>
-      <Navbar />
-      <Routes basename={process.env.PUBLIC_URL}>
-      <Route exact path="/" Component={Home}></Route>
-      <Route path="/apply" element={<Apply />}></Route>
-       </Routes>
-       <OurInfo />
-      <Footer />
-      <LastFooter />
-
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apply" element={<Apply />} />
+        </Routes>
+        <OurInfo />
+        <Footer />
+        <LastFooter />
       </DataProvider>
-     
-
-    </div>
+    </HashRouter>
   );
 }
 
